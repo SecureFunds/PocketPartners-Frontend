d05ac9979d07404f58da74a5ccaf2f3045966a8c
Nadia <nad21lc@gmail.com>
2024-10-23 16:28:09 -0500

    feat: add firebase files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
   baseURL: 'http://localhost:8080/api/v1',
+  firebaseConfig: {
+    apiKey: "AIzaSyA4RzVQ5SXfExHEwo_0JAWZcn7q-rZxq7w",
+    authDomain: "agroconnect-demo-8c7ab.firebaseapp.com",
+    projectId: "agroconnect-demo-8c7ab",
+    storageBucket: "agroconnect-demo-8c7ab.appspot.com",
+    messagingSenderId: "617590434096",
+    appId: "1:617590434096:web:9957e257ce9221b7bc00f5",
+    measurementId: "G-004GHXNHG9"
+  }
 };
